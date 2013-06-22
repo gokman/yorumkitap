820b20997450431bfeb1111e5ea2d0f2838d6f9a
fatihyesildal@gmail.com <fatihyesildal@gmail.com@d68d25c8-a1b5-e43c-2a51-72b3f15b49a6>
2013-06-22 20:39:37 +0000

    Giriş işlemi için "Beni hatırla" fonksiyonu eklendi.

--- com.bookworm.main.MainActivity/src/com/bookworm/main/LoginActivity.java
+++ com.bookworm.main.MainActivity/src/com/bookworm/main/LoginActivity.java
@@ -3,6 +3,7 @@ package com.bookworm.main;
 import java.util.concurrent.ExecutionException;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,31 +15,34 @@ import android.view.View.OnClickListener;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
-import android.widget.ImageView;
-import android.widget.RelativeLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import com.bookworm.common.ApplicationConstants;
 import com.bookworm.test.DataHelper;
 import com.bookworm.test.MyApplication;
 import com.netmera.mobile.NetmeraClient;
 import com.netmera.mobile.NetmeraContent;
 import com.netmera.mobile.NetmeraException;
 import com.netmera.mobile.NetmeraUser;
-
 public class LoginActivity extends ActivityBase implements OnClickListener {
 
 	static String resultEmail;
 	static NetmeraContent temp;
 	boolean registered = false;
 
+	public static String filename = "let_the_books_begin";
+	private SharedPreferences SP;
 	private DataHelper dataHelper;
-
-	private static final String NAME = "NAME";
 
 	private EditText _email;
 	private EditText _password;
+	private CheckBox _rememberMe;
+	private CheckBox _forgotPassword;
+	private String savedUsername = ApplicationConstants.EMPTY_STRING;
+	private String savedPassword = ApplicationConstants.EMPTY_STRING;;
 	
 	/** Called when the activity is first created. */
 	@Override
@@ -51,8 +55,25 @@ public class LoginActivity extends ActivityBase implements OnClickListener {
 		setContentView(R.layout.login);
 		getWindow().setFeatureInt(Window.FEATURE_CUSTOM_TITLE, R.layout.window_title);
 
+		SP = getSharedPreferences(filename, 0);
+
+		savedUsername = SP.getString(ApplicationConstants.username,ApplicationConstants.unregistered_username);
+		savedPassword = SP.getString(ApplicationConstants.password,ApplicationConstants.unregistered_password);
+		
+		_rememberMe = (CheckBox) findViewById(R.id.rememberMe);
+		_forgotPassword = (CheckBox) findViewById(R.id.forgotPassword);
+
+		_forgotPassword.setChecked(false);
+		
 		_email = (EditText)findViewById(R.id.emailText);
 		_password = (EditText)findViewById(R.id.passwordText);
+		
+		if(!savedUsername.equals(ApplicationConstants.unregistered_username)){
+			_email.setText(savedUsername);
+		}
+		if(!savedPassword.equals(ApplicationConstants.unregistered_password)){
+			_password.setText(savedPassword);
+		}		
 		
 		// Listening to register new account link
 		TextView registerScreen = (TextView) findViewById(R.id.link_to_register);
@@ -82,10 +103,43 @@ public class LoginActivity extends ActivityBase implements OnClickListener {
 							"Email or password is wrong", Toast.LENGTH_LONG)
 							.show();
 				} else {
+					/*
+					 * Eger login olan kullanici;
+					 * sifresi kayitli olan kullanýcýysa ve "beni hatirla" secenegi secilmemisse
+					 * kayitli sifre ve kullanici bilgileri
+					 * else kisminda siliniyor.
+					 */
+					if(_rememberMe.isChecked()){
+						String _emailToStore = _email.getText().toString();
+						String _passwordToStore = _password.getText().toString();
+
+						SharedPreferences.Editor editit = SP.edit();
+						editit.putString(ApplicationConstants.username, _emailToStore);
+						editit.putString(ApplicationConstants.password,_passwordToStore);
+						editit.commit();
+
+					}else{
+						if(savedUsername.equals(_email.getText().toString())){
+							SharedPreferences.Editor editit = SP.edit();
+							editit.remove(ApplicationConstants.username);
+							editit.remove(ApplicationConstants.password);
+							editit.commit();
+						}
+					}
 					Intent mainPageIntent = new Intent(getApplicationContext(),
 							MainActivity.class);
 					startActivity(mainPageIntent);
 				}		
+			}
+		});
+		
+		_forgotPassword.setOnClickListener( new OnClickListener() {
+			
+			public void onClick(View v) {
+
+				if(((CheckBox)v).isChecked()){
+					
+				}
 			}
 		});
 
