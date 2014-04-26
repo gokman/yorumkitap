5c190c303d2f83617283661c6179dc5328a1ee43
gokhankcmn@gmail.com <gokhankcmn@gmail.com@d68d25c8-a1b5-e43c-2a51-72b3f15b49a6>
2014-04-26 21:01:33 +0000

    booklike web servis sil bastan yazildi

--- com.bookworm.main.MainActivity/src/com/bookworm/ws/like/GetBookLikeWS.java
+++ com.bookworm.main.MainActivity/src/com/bookworm/ws/like/GetBookLikeWS.java
@@ -2,15 +2,21 @@ package com.bookworm.ws.like;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 
 import org.apache.http.HttpResponse;
 import org.apache.http.client.HttpClient;
 import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPost;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONObject;
 
 import android.os.AsyncTask;
+import android.util.Base64;
 
+import com.bookworm.common.ApplicationConstants;
 import com.bookworm.common.Utils;
 import com.bookworm.model.BookLike;
 
@@ -34,18 +40,39 @@ public class GetBookLikeWS extends
 
 			// create HttpClient
 			HttpClient httpclient = new DefaultHttpClient();
+			
+			 // make GET request to the given URL
+            HttpGet get=new HttpGet(url);
+            get.setHeader("Accept", "application/json");
+            get.setHeader("Content-Type", "application/json");
+            
+            //credentials
+            String credentials = ApplicationConstants.signed_in_email + ":" + ApplicationConstants.signed_in_password;  
+            String base64EncodedCredentials = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);  
+            get.addHeader("Authorization", "Basic " + base64EncodedCredentials);
 
 			// make GET request to the given URL
-			HttpResponse httpResponse = httpclient.execute(new HttpGet(url));
+			HttpResponse httpResponse = httpclient.execute(get);
 
 			// receive response as inputStream
 			inputStream = httpResponse.getEntity().getContent();
 			String result = "";
 			// convert inputstream to string
 			if (inputStream != null)
-				 Utils.convertInputStreamToString(inputStream);
+				 result=Utils.convertInputStreamToString(inputStream);
 			else
 				 result = "Did not work!";
+			
+			JSONArray resultArray = new JSONArray(result);
+			for (int k = 0 ; k < resultArray.length(); k++){
+				JSONObject obj = resultArray.getJSONObject(k);
+				BookLike bookLike = new BookLike();
+				bookLike.setBookId(obj.getLong("bookId"));
+				bookLike.setBookLikeId(obj.getLong("bookLikeId"));
+				bookLike.setBookLikerId(obj.getLong("bookLikerId"));
+				//bookLike.setBookLikeDate(new Date());
+				resultList.add(bookLike);
+			}
 
 		} catch (Exception e) {
 			e.getLocalizedMessage();
