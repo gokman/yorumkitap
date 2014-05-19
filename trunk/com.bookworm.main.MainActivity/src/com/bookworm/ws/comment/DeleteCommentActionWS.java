ac71e387ddcfccdf8fe56b634c192133ebcbecbb
gokhankcmn@gmail.com <gokhankcmn@gmail.com@d68d25c8-a1b5-e43c-2a51-72b3f15b49a6>
2014-05-19 12:17:26 +0000

    comment silme ws sınıfı eklendi

---
+++ trunk/com.bookworm.main.MainActivity/src/com/bookworm/ws/comment/DeleteCommentActionWS.java
@@ -0,0 +1,78 @@
+package com.bookworm.ws.comment;
+
+import java.io.InputStream;
+
+
+import org.apache.http.HttpResponse;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.entity.StringEntity;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.message.BasicHeader;
+import org.apache.http.protocol.HTTP;
+import org.json.JSONObject;
+import android.os.AsyncTask;
+import android.util.Base64;
+import com.bookworm.common.ApplicationConstants;
+import com.bookworm.common.Utils;
+import com.bookworm.model.BookLike;
+import com.bookworm.model.Comment;
+
+public class DeleteCommentActionWS extends AsyncTask<Object, Void, String> {
+		
+        @Override
+        protected String doInBackground(Object... args) {
+        	return POST((String)args[0],(Comment)args[1]);
+        }
+        
+        @Override
+        protected void onPostExecute(String result) {
+
+					
+       }
+    	public static String POST(String url,Comment comment){
+
+    	       InputStream inputStream = null;
+    	        String result = "";
+    	        BookLike actionReturned = null;
+    	        try {
+    	            // create HttpClient
+    	            HttpClient httpclient = new DefaultHttpClient();
+
+    	            // make GET request to the given URL
+    	            HttpPost post=new HttpPost(url);
+    	            post.setHeader("Accept", "application/json");
+    	            post.setHeader("Content-Type", "application/json");
+    	            
+    	            //credentials
+    	            String credentials = ApplicationConstants.signed_in_email + ":" + ApplicationConstants.signed_in_password;  
+    	            String base64EncodedCredentials = Base64.encodeToString(credentials.getBytes(), Base64.NO_WRAP);  
+    	            post.addHeader("Authorization", "Basic " + base64EncodedCredentials);
+    	            
+    	            //TODO Aksiyon bilgileri set edilicek
+    	            JSONObject jsonum=new JSONObject();
+                    jsonum.put("commentId", comment.getCommentId());
+    	            StringEntity sampleEntity=new StringEntity(jsonum.toString());
+                    sampleEntity.setContentType(new BasicHeader(HTTP.CONTENT_TYPE, "application/json"));
+
+    	            post.setEntity(sampleEntity);
+    	            HttpResponse httpResponse = httpclient.execute(post);
+    	 
+    	            // receive response as inputStream
+    	            inputStream = httpResponse.getEntity().getContent();
+    	            // convert inputstream to string
+    	            if(inputStream != null)
+    	                result = Utils.convertInputStreamToString(inputStream);
+    	            else
+    	                result = "Did not work!";
+    	            
+    	        } catch (Exception e) {
+    	            e.getLocalizedMessage();
+    	        }
+    	        return result;
+        }
+    	public void postExecuteForPost(String result){
+            
+    	}
+    			    
+    }
